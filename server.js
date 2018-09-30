adcdd1fcad166387dbb4f7d914ef961cc8992e75
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2018-09-29 23:03:32 -0600

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use('/public', express.static('public'));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.listen(8080);
